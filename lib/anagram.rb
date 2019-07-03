7ebfd63c6f2b422f81363225ebeda02b1ff57e1d
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-03 18:33:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
 
   def match(string)
    match = []
-    @string = string.split
+    @string = string.split(", ")
    @string.each do |word|
      match << word if word.split("").sort == @word.split("").sort
    end
